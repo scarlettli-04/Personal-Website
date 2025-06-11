0e53b2c21e37a68d8bc24a688f35e26f354a69b3
scarlettli-04 <scarlettsaur@gmail.com>
2025-06-10 21:14:35 -0400

    dropdown on about me page

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// ABOUT ME dropdown's
+document.addEventListener('DOMContentLoaded', () => {
+    const amContainerleft = document.querySelector('.am-content-left');
+    const amContainerright = document.querySelector('.am-content-right');
+
+    // LEFT SIDE
+    amContainerleft.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.am-group-header');
+
+        if(!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.am-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        // Toggle icon
+        icon.classList.toggle('fa-angle-up');
+        icon.classList.toggle('fa-angle-down');
+
+        // Toggle visibility of body text
+        groupBody.classList.toggle('open');
+    });
+
+    // RIGHT SIDE
+    amContainerright.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.am-group-header');
+
+        if(!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.am-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        // Toggle icon
+        icon.classList.toggle('fa-angle-up');
+        icon.classList.toggle('fa-angle-down');
+
+        // Toggle visibility of body text
+        groupBody.classList.toggle('open');
+    });
+});
